d134f1c2b52c6251096b6dbcfbcdd202ac6e9e70
Daniel_MQ <danielmq117@gmail.com>
2024-07-20 19:26:22 -0600

    Add: Archivo de paleta de colores para el layout y mejorar la UX

--- Production.py
+++ Production.py
@@ -5,6 +5,7 @@ import threading
 import ffmpeg as fm
 from PIL import Image
 from requests import *
+from Theme import colors
 from pathlib import Path
 from datetime import date
 from pytube import YouTube
@@ -13,15 +14,15 @@ from threading import Thread
 from bs4 import BeautifulSoup
 from tkinter import StringVar
 from collections import Counter
-from os import readlink, remove
 from pytube.exceptions import *
+from os import readlink, remove
 from typing import Dict, Literal
 from time import strftime, localtime
 
 # prevent IDE from removing unused imports
 _ = re, StringVar, fm, YouTube, Image, readlink, remove, date, Ctk, strftime, localtime, BeautifulSoup
 
-Ctk.set_appearance_mode('dark')
+Ctk.set_appearance_mode('light')
 Ctk.set_default_color_theme('green')
 
 
@@ -117,7 +118,9 @@ class Download_File():
 
     def saveFile(self, title, outputPath, **kwargs):
 
-        if existsThread('Thread-2'):
+        exists = existsThread('Thread-2')
+
+        if not exists:
             self.threadSave = Thread(target=self.outputFile, name='Thread-2', args=(title, outputPath), kwargs=kwargs, daemon=True) 
             self.threadSave.start()
         else:
@@ -142,88 +145,96 @@ class App(Ctk.CTk):
         self.resizable(False, 0)            # type: ignore
         self.title('Video Downloader')
 
-        self.font = Ctk.CTkFont(family='Roboto',size=12, weight='bold', slant='roman')
+        self.fontR = Ctk.CTkFont(family='Roboto',size=13, weight='bold', slant='roman')
+        self.fontV = Ctk.CTkFont(family='Verdana',size=12, weight='normal', slant='roman')
+        self.fontI = Ctk.CTkFont(family='Verdana', size=11, weight='normal', slant='italic')
 
-        self.root0Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color='gray84', height=100)
+        self.root0Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color=colors[0], height=100)
         self.root0Frame.grid(row=0, column=0, sticky=Ctk.NSEW, padx=10, pady=(10, 5))
 
-        self.root1Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color='gray84', height=100)
+        self.root1Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color=colors[0], height=100)
         self.root1Frame.grid(row=1, column=0, sticky=Ctk.NSEW, padx=10, pady=(5, 5))
 
-        self.root2Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color='gray84', height=100)
+        self.root2Frame = Ctk.CTkFrame(self, corner_radius=6, fg_color=colors[0], height=100)
         self.root2Frame.grid(row=2, column=0, sticky=Ctk.NSEW, padx=10, pady=(5, 10))
 
-        self.URLLabel = Ctk.CTkLabel(self.root0Frame, width=51, height=self.height, text='URL', font=self.font, fg_color='#71C5E8', corner_radius=6)
+        self.URLLabel = Ctk.CTkLabel(self.root0Frame, width=51, height=self.height, text='URL: :', text_color=colors[2], font=self.fontR, fg_color=colors[5], corner_radius=6)
         self.URLLabel.grid(row=0, column=0, padx=(10, 5), pady=(10, 10), sticky=Ctk.W)
 
-        self.entryURL = Ctk.CTkEntry(self.root0Frame, placeholder_text='Copy the URL here', text_color='#19222B', font=Ctk.CTkFont(size=12, weight='normal', slant='italic'), width=200, height=34, corner_radius=6, fg_color='#EBBDD1')
+        self.entryURL = Ctk.CTkEntry(self.root0Frame, placeholder_text='Copy the URL here', text_color=colors[4], font=self.fontI, width=200, height=34, corner_radius=6, fg_color=colors[1], border_color=colors[5])
         self.entryURL.grid(row=0, column=1, padx=(5, 5), pady=(10, 10), sticky=Ctk.NSEW)
         self.entryURL.bind('<Return>', self.clickEvent)
 
-        self.clearButton = Ctk.CTkButton(self.root0Frame, text='x', width=34, height=34, text_color='gray10', bg_color='transparent', fg_color='gray84', hover_color='gray78', command=self.clearUrl)
+        self.clearButton = Ctk.CTkButton(self.root0Frame, text='x', font=self.fontR, width=34, height=34, text_color=colors[3], fg_color=colors[0], hover_color=colors[5], command=self.clearUrl)
         self.clearButton.grid(row=0, column=2, padx=(5, 10), pady=(10, 10), sticky=Ctk.NSEW)
 
-        self.titleLabel = Ctk.CTkLabel(self.root1Frame, width=51, height=self.height, text='Title:', font=self.font, fg_color='#71C5E8', corner_radius=6)
+        self.titleLabel = Ctk.CTkLabel(self.root1Frame, width=51, height=self.height, text='Title:', text_color=colors[2], font=self.fontR, fg_color=colors[7], corner_radius=6)
         self.titleLabel.grid(row=0, column=0, padx=(10, 5), pady=(10, 5), sticky=Ctk.NSEW)
 
         self.title = StringVar()
-        self.titleTextbox = Ctk.CTkEntry(master=self.root1Frame, width=200, height=self.height, corner_radius=6, fg_color='gray78', text_color='#000000')
+        self.titleTextbox = Ctk.CTkEntry(master=self.root1Frame, width=200, height=self.height, corner_radius=6, fg_color=colors[1], text_color=colors[3], border_color=colors[7])
         self.titleTextbox.grid(row=0, column=1, padx=(5, 5), pady=(10, 5), sticky='nsew')
-        self.titleTextbox.configure(state='readonly')
+        self.titleTextbox.configure(state='readonly', font=self.fontV)
         
         self.duration = StringVar()
-        self.durationLabel = Ctk.CTkLabel(master=self.root1Frame, text='00:00' ,font=Ctk.CTkFont(size=12, weight='normal', slant='roman'), width=34, height=self.height, corner_radius=6, fg_color='gray78', text_color='#000000')
+        self.durationLabel = Ctk.CTkLabel(master=self.root1Frame, text='00:00', font=self.fontV, width=34, height=self.height, corner_radius=6, fg_color=colors[1], text_color=colors[3])
         self.durationLabel.grid(row=0, column=2, padx=(5, 10), pady=(10, 5), sticky='nsew')
 
-        self.autorLabel = Ctk.CTkLabel(self.root1Frame, width=51, height=self.height, text='Autor:', font=self.font, fg_color='#71C5E8', corner_radius=6)
+        self.autorLabel = Ctk.CTkLabel(self.root1Frame, width=51, height=self.height, text='Autor:', text_color=colors[2], font=self.fontR, fg_color=colors[7], corner_radius=6)
         self.autorLabel.grid(row=1, column=0, padx=(10, 5), pady=(5, 10), sticky=Ctk.W)
 
         self.autor = StringVar()
-        self.autorTextbox = Ctk.CTkEntry(master=self.root1Frame, width=200, height=self.height, corner_radius=6, fg_color='gray78', text_color='#000000')
+        self.autorTextbox = Ctk.CTkEntry(master=self.root1Frame, width=200, height=self.height, corner_radius=6, fg_color=colors[1], text_color=colors[3], border_color=colors[7])
         self.autorTextbox.grid(row=1, column=1, padx=(5, 5), pady=(5, 10), sticky='nsew')
-        self.autorTextbox.configure(state='readonly')
+        self.autorTextbox.configure(state='readonly', font=self.fontV)
 
-        self.setPathButton = Ctk.CTkButton(master=self.root1Frame, width=60, height=34, text='Save in', font=self.font, fg_color='#71C5E8', command=self.setPath)
+        self.setPathButton = Ctk.CTkButton(master=self.root1Frame, width=60, height=34, text='Save in', text_color=colors[2], font=self.fontR, fg_color=colors[9], hover_color=colors[10],command=self.setPath)
         self.setPathButton.grid(row=2, column=1, padx=(5, 10), pady=(5, 5), sticky='w')
 
         self.downloadIcon = Ctk.CTkImage(light_image=Image.open(fp="./Icons/Download_Icon.png"), size=(26,26))
 
-        self.downloadVideo = Ctk.CTkButton(master=self.root1Frame, width=110, height=34, corner_radius=17, text_color='#000000', text='Download', font=self.font, image=self.downloadIcon, fg_color='#8a2be2', command=self.saveFile, compound=Ctk.RIGHT)
+        self.downloadVideo = Ctk.CTkButton(master=self.root1Frame, width=110, height=34, corner_radius=17, text='Download', font=self.fontR, image=self.downloadIcon, fg_color=colors[8], command=self.saveFile, compound=Ctk.RIGHT)
         self.downloadVideo.configure(state='disabled')
         self.downloadVideo.grid(row=2, column=1, padx=(5, 5), pady=(5, 5), sticky='e')
 
-        self.parametersTabview = Ctk.CTkTabview(self.root1Frame, height=40, anchor='NW')
+        self.parametersTabview = Ctk.CTkTabview(self.root1Frame, height=90, anchor='NW', fg_color=colors[1], segmented_button_fg_color=colors[9], segmented_button_selected_color=colors[9], segmented_button_selected_hover_color=colors[9])
+        self.parametersTabview._segmented_button.configure(font=self.fontR)
         self.parametersTabview.grid(row=3, column=0, padx=(10, 10), pady=(10, 10), columnspan=3, sticky='nsew')
-        self.parametersTabview.add('Set Quality')
+        self._tabName = 'SETTINGS'
+        self.parametersTabview.add(self._tabName)
+        self.parametersTabview._segmented_button._buttons_dict[self._tabName].configure(cursor='arrow')
 
         self.videoCombobox_var = Ctk.StringVar()
         self.videoItag = []
-        self.videoCombobox = Ctk.CTkComboBox(self.parametersTabview.tab('Set Quality'), values=self.videoItag, width=90,command=video_Combobox_callback, variable=self.videoCombobox_var)
+        self.videoCombobox = Ctk.CTkComboBox(self.parametersTabview.tab(self._tabName), values=self.videoItag, width=80, command=video_Combobox_callback, variable=self.videoCombobox_var, fg_color=colors[1], font=self.fontV)
         self.videoCombobox.grid(row=0, column=0, padx=(10, 5), pady=(5, 10), sticky=Ctk.W)
         self.videoCombobox.configure(state='disabled')
+        self.videoCombobox._dropdown_menu.configure(fg_color=colors[6], font=self.fontV)
         self.videoCombobox_var.set('Video')
 
         self.audioCombobox_var = Ctk.StringVar()
         self.showAudioItag = []
-        self.audioCombobox = Ctk.CTkComboBox(self.parametersTabview.tab('Set Quality'), values=self.showAudioItag, width=90, command=audio_Combobox_callback, variable=self.audioCombobox_var)
+        self.audioCombobox = Ctk.CTkComboBox(self.parametersTabview.tab(self._tabName), values=self.showAudioItag, width=95, command=audio_Combobox_callback, variable=self.audioCombobox_var, fg_color=colors[1], font=self.fontV)
         self.audioCombobox.grid(row=0, column=1, padx=(5, 5), pady=(5, 10), sticky=Ctk.N)
         self.audioCombobox.configure(state='disabled')
+        self.audioCombobox._dropdown_menu.configure(fg_color=colors[6], font=self.fontV)
         self.audioCombobox_var.set('Audio')
 
         self.videoFormatCombobox_var = Ctk.StringVar()
-        self.videoFormatCombobox = Ctk.CTkComboBox(self.parametersTabview.tab('Set Quality'), width=80, values=self.videoFormatItag, command=videoFormat_Combobox_callback, variable=self.videoFormatCombobox_var)
+        self.videoFormatCombobox = Ctk.CTkComboBox(self.parametersTabview.tab(self._tabName), width=90, values=self.videoFormatItag, command=videoFormat_Combobox_callback, variable=self.videoFormatCombobox_var, fg_color=colors[0], font=self.fontV)
         self.videoFormatCombobox.grid(row=0, column=2, padx=(5, 10), pady=(5, 10), sticky=Ctk.E)
-        self.videoFormatCombobox.configure(state='readonly')
-        self.videoFormatCombobox_var.set('format')
+        self.videoFormatCombobox.configure(state='readonly', button_color=colors[5], border_color=colors[5])
+        self.videoFormatCombobox._dropdown_menu.configure(fg_color=colors[6], font=self.fontV)
+        self.videoFormatCombobox_var.set('.format')
 
         self.videoIcon = Ctk.CTkImage(light_image=Image.open(fp="./Icons/Video_Icon.png"), size=(100,100))
 
         self.videoImage = Ctk.CTkLabel(self.root2Frame, text='', compound='center', image=self.videoIcon)
         self.videoImage.pack(fill='x', anchor='center', side=Ctk.BOTTOM, padx=(10, 10), pady=(10, 10))
 
-        self.loadingPreviewInfo = Ctk.CTkProgressBar(self.root2Frame, width=310, height=8, corner_radius=4, orientation='horizontal', mode='determinate', determinate_speed=20)
+        self.loadingPreviewInfo = Ctk.CTkProgressBar(self.root2Frame, width=310, height=8, corner_radius=4, orientation='horizontal', mode='determinate', determinate_speed=20, progress_color=colors[9])
         self.loadingPreviewInfo.set(value=0)
-        
+
         self.downloader = Download_File()
 
         #region METHODS
@@ -244,7 +255,7 @@ class App(Ctk.CTk):
 
         self.videoCombobox_var.set('Video')
         self.audioCombobox_var.set('Audio')
-        self.videoFormatCombobox_var.set('format')
+        self.videoFormatCombobox_var.set('.format')
 
         self.videoCombobox.configure(values=[])
         self.audioCombobox.configure(values=[])
@@ -254,9 +265,13 @@ class App(Ctk.CTk):
 
     def swichtWidgetState(self, state: str) -> None:
 
-        self.downloadVideo.configure(state=state)
-        self.videoCombobox.configure(state=state)
-        self.audioCombobox.configure(state=state)
+        if state == 'readonly':
+            fg_c1=colors[7]; fg_c2=colors[0]; cursor='hand2'; bbc=colors[5]
+        else: fg_c1=colors[8]; fg_c2=colors[1]; cursor='arrow'; bbc=colors[11]
+
+        self.downloadVideo.configure(state=state, fg_color=fg_c1, cursor=cursor)
+        self.videoCombobox.configure(state=state, fg_color=fg_c2, button_color=bbc, border_color=bbc)
+        self.audioCombobox.configure(state=state, fg_color=fg_c2, button_color=bbc, border_color=bbc)
 
     def convertToMinutes(self, duration: int = 0) -> str:
 
@@ -269,7 +284,9 @@ class App(Ctk.CTk):
     
     def clickEvent(self, event):
 
-        if existsThread('Thread-1'):
+        exists = existsThread('Thread-1')
+
+        if not exists:
             self.threadDownload = Thread(target=self.readURL, name='Thread-1', daemon=True) 
             self.threadDownload.start()
         else:
@@ -406,7 +423,7 @@ class App(Ctk.CTk):
         v = self.videoCombobox.get()
         f = self.videoFormatCombobox.get()
 
-        if f == 'format': f = '.mp3'
+        if f == '.format': f = '.mp3'
 
         if a == 'Audio':
             a = self.audioItag[-2]
---
+++ Theme.py
@@ -0,0 +1,28 @@
+
+section = 'gray84'
+desactivate = 'gray78'
+text_light = '#fff'
+text_dark = '#000'
+text_entry_ph = '#19222B'
+orange = '#F26350'
+yellow = '#FCD462'
+blueviolet = '#8A2BE2'
+blueviolet_hvc = '#551B8C'
+green = '#52B36E'
+green_hcv = '#0C955A'
+active = '#979DA2'
+
+colors = [
+    section,
+    desactivate,
+    text_light,
+    text_dark,
+    text_entry_ph,
+    orange,
+    yellow,
+    blueviolet,
+    blueviolet_hvc,
+    green,
+    green_hcv,
+    active
+]
